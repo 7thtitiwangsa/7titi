8641eae67544e2ee922225ceb6625d1db1643798
7thtitiwangsa <7thtitiwangsa@gmail.com>
2025-11-04 20:46:44 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['nocodb-production-c4d4.up.railway.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
   env: {
     NEXT_PUBLIC_NOCODB_URL: 'https://nocodb-production-c4d4.up.railway.app',
